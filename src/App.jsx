b7bd26885b2d4d198d4fb9100fc5804a6ee0bf22
Marcos2611 <marcos.ortuno@estudiantat.upc.edu>
2025-11-25 12:11:03 +0100

    Routes(contiene errores aun)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import Search from './pages/Search'
 import Footer from './components/Footer'
 import './App.css'
 
@@ -9,9 +11,14 @@ function App() {
   
 
   return (
-    <>   
-     <HomePage />
-     <Footer />
+    <> 
+     <Routes>
+        <Route path="/pages/HomePage" element={<HomePage />} />
+        <Route path="/pages/Search" element={<Search />} />
+      </Routes>
+   
+    <Footer />
+     
     </>
   )
 }
