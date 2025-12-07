379aef2895eef42716b579e11e5eb8af91ac5dec
Marcos2611 <marcos.ortuno@estudiantat.upc.edu>
2025-12-07 12:57:29 +0100

    dsf

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
-import { useEffect } from 'react'
-import './Footer.css'
+import { Link } from 'react-router-dom'; // Importa Link de react-router-dom
+import './Footer.css';
 import Home from '../assets/icons/home.png';
 import Search from '../assets/icons/Search.png';
 import mas from '../assets/icons/mas.png';
@@ -8,19 +7,25 @@ import User from '../assets/icons/User.png';
 import Diario from '../assets/icons/Diario.png';
 
 function Footer() {
-  
-
   return (
     <footer className="footer-container">
-      <a href="../pages/HomePage" className="footer-btn"><img src={Home} alt="Home" className="footer-icon" /> </a>
-      <a href="../pages/Search" className="footer-btn"><img src={Search} alt="Search" className="footer-icon" /> </a>
-      <a href="#" className="footer-btn"><img src={mas} alt="mas" className="footer-icon" /> </a>
-      <a href="#" className="footer-btn"><img src={Diario} alt="Diario" className="footer-icon" /> </a>
-      <a href="#" className="footer-btn"><img src={User} alt="User" className="footer-icon" /> </a>
-
-
+      <Link to="/" className="footer-btn"> {/* Usa Link en lugar de <a> */}
+        <img src={Home} alt="Home" className="footer-icon" />
+      </Link>
+      <Link to="/search" className="footer-btn">
+        <img src={Search} alt="Search" className="footer-icon" />
+      </Link>
+      <Link to="#" className="footer-btn">
+        <img src={mas} alt="mas" className="footer-icon" />
+      </Link>
+      <Link to="#" className="footer-btn">
+        <img src={Diario} alt="Diario" className="footer-icon" />
+      </Link>
+      <Link to="#" className="footer-btn">
+        <img src={User} alt="User" className="footer-icon" />
+      </Link>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
