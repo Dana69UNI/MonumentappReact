79b1d91a216a17c9b30cbc347cd7a7b939e51632
Marcos2611 <marcos.ortuno@estudiantat.upc.edu>
2025-12-07 14:12:12 +0100

    más cambios

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,86 @@
-import { Link } from 'react-router-dom'; // Importa Link de react-router-dom
-import './Footer.css';
-import Home from '../assets/icons/home.png';
-import Search from '../assets/icons/Search.png';
-import mas from '../assets/icons/mas.png';
-import User from '../assets/icons/User.png';
-import Diario from '../assets/icons/Diario.png';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
-function Footer() {
+import './Footer.css'
+
+//IMPORTAR IMATGES
+import IconHome from '../assets/icons/Home.svg';
+import IconSearch from '../assets/icons/Search.svg';
+import IconNew from '../assets/icons/New.svg';
+import IconBiblioteca from '../assets/icons/Tree.svg';
+import IconProfile from '../assets/icons/User.svg';
+
+const Footer = () => {
+  const location = useLocation();
+
+  const menuItems = [
+    {
+      name: 'Inici',
+      path: '/',
+      icon: IconHome //cada icona amb la variable
+    },
+    {
+      name: 'Cercar',
+      path: '/cercar',
+      icon: IconSearch
+    },
+    { 
+      name: 'Nou',
+      path: '/nou',
+      icon: IconNew
+    },
+    {
+      name: 'Biblioteca',
+      path: '/biblioteca', 
+      icon: IconBiblioteca
+    },
+    {
+      name: 'Perfil',
+      path: '/perfil',
+      icon: IconProfile
+    }
+  ];
+
   return (
-    <footer className="footer-container">
-      <Link to="/" className="footer-btn"> {/* Usa Link en lugar de <a> */}
-        <img src={Home} alt="Home" className="footer-icon" />
-      </Link>
-      <Link to="/search" className="footer-btn">
-        <img src={Search} alt="Search" className="footer-icon" />
-      </Link>
-      <Link to="#" className="footer-btn">
-        <img src={mas} alt="mas" className="footer-icon" />
-      </Link>
-      <Link to="#" className="footer-btn">
-        <img src={Diario} alt="Diario" className="footer-icon" />
-      </Link>
-      <Link to="#" className="footer-btn">
-        <img src={User} alt="User" className="footer-icon" />
-      </Link>
-    </footer>
+    <nav className="footer-nav">
+      <ul className="footer-list">
+        {menuItems.map((item) => {
+          
+          let isActive = false;
+
+          if (item.path === '/') {
+            //CAS HOME: Ha de ser idèntic (===), si no s'activaria amb tot
+            isActive = location.pathname === '/';
+          } 
+          
+          else {
+            //RESTA DE CASOS: Si estem a /biblioteca/roure, volem que /biblioteca s'activi
+            isActive = location.pathname.startsWith(item.path);
+          }
+
+          return (
+            <li key={item.name} className={`footer-item ${isActive ? 'active' : ''}`}>
+              <Link to={item.path} className="footer-link">
+                
+                {/*Imatges footer en format svg*/}
+               <div 
+                className="footer-icon-mask"
+                style={{
+                  // Passem la ruta de l'SVG a la propietat mask-image
+                  WebkitMaskImage: `url(${item.icon})`,
+                  maskImage: `url(${item.icon})`
+                }}
+              />
+
+                {/*Això serveix per si hi volguessim textos*/}
+                {/* <span className="label">{item.name}</span> */}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
   );
-}
+};
 
 export default Footer;
